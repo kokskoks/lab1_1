f99e27bf9ae1bcce9f4c65700a679fb3a44fcbdb
Kadłubek <179640@edu.p.lodz.pl>
2016-03-17 18:42:04 +0100

    Updated documentation of the ValueComparator interface

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/ValueComparator.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/ValueComparator.java
@@ -2,9 +2,9 @@ package pl.com.bottega.ecommerce.sales.domain.offer;
 
 public interface ValueComparator<K> {
 	/**
-	 * 
-	 * @param seenOffer
-	 * @param delta acceptable difference in percent
+	 * Method to compare two K object with some wiggle room for price difference
+	 * @param seenOffer K object to compare with
+	 * @param delta acceptable difference of the K price in percent
 	 * @return
 	 */
 	boolean sameAs(K item, double priceDelta);
