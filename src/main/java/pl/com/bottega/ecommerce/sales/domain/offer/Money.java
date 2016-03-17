a4a00f9c3a220c02e709c679889d00c1c29cf2a4
Kadłubek <179640@edu.p.lodz.pl>
2016-03-17 17:32:49 +0100

    extracted Money class from OfferItem class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -0,0 +1,27 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.math.BigDecimal;
+
+public class Money {
+	private BigDecimal totalCost;
+	private String currency;
+
+	public Money() {
+	}
+
+	public BigDecimal getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(BigDecimal totalCost) {
+		this.totalCost = totalCost;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+}
