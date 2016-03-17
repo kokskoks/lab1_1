dba19ecacf40169ddde4f0aeab609751de894386
Kadłubek <179640@edu.p.lodz.pl>
2016-03-17 18:22:06 +0100

    Added hashCode and equals override in Discount class

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -29,5 +29,38 @@ public class Discount {
 		this.money = money;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((discountCause == null) ? 0 : discountCause.hashCode());
+		result = prime * result + ((money == null) ? 0 : money.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Discount other = (Discount) obj;
+		if (discountCause == null) {
+			if (other.discountCause != null)
+				return false;
+		} else if (!discountCause.equals(other.discountCause))
+			return false;
+		if (money == null) {
+			if (other.money != null)
+				return false;
+		} else if (!money.equals(other.money))
+			return false;
+		return true;
+	}
+	
+	
+
 	
 }
